fccba281593c00eea9739629a8ed38353771c4c8
egijago <13521056@mahasiswa.itb.ac.id>
2023-10-18 08:54:10 +0700

    feat: restructure code infrastructure

--- src/Main/GameState.java
+++ src/Main/GameState.java
@@ -1,7 +1,7 @@
+package Main;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class GameState {
     public static int ROW = 8;
@@ -11,7 +11,7 @@ public class GameState {
 
     int roundsLeft;
 
-    String[][] node;
+    public String[][] node;
 
     public GameState(OutputFrameController gameBoard) {
         this.gameBoard = gameBoard;
@@ -90,53 +90,7 @@ public class GameState {
         return 4 * (countFixedO - countFixedX) + 1 * (countVolatileO - countVolatileX);
     }
 
-//    public int evaluate() {
-//        int countX = 0;
-//        int countO = 0;
-//        int countBadX = 0;
-//        int countBadO = 0;
-//        Map <int[], Boolean> cache = new HashMap<>();
-//        int[][] badVector = new int[][] {{-2, 0}, {2, 0}, {0, -2}, {0, 2}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-//        for (int i = 0; i < ROW; i++) {
-//            for (int j = 0; j < COL; j++) {
-//                if (node[i][j].equals("X")) {
-//                    countX++;
-//                    for (int[] vector: badVector) {
-//                        int _i = i + vector[0];
-//                        int _j = j + vector[1];
-//                        int[] coor = new int[]{_i, _j};
-//                        if (!isNodeValid(_i, _j) || cache.containsKey(coor)) {
-//                          continue;
-//                        };
-//                        cache.put(coor, true);
-//                        if (node[_i][_j].equals("X")) {
-//                            countBadX ++;
-//                        }
-//                    }
-//                } else if (node[i][j].equals("O")) {
-//                    countO++;
-//                    for (int[] vector: badVector) {
-//                        int _i = i + vector[0];
-//                        int _j = j + vector[1];
-//                        int[] coor = new int[]{_i, _j};
-//                        if (!isNodeValid(_i, _j) || cache.containsKey(coor)) {
-//                            continue;
-//                        };
-//                        cache.put(coor, true);
-//                        if (node[_i][_j].equals("O")) {
-//                            countBadO ++;
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        int w1 = 2;
-//        int w2 = 1;
-//        int f1 = countX - countO;
-//        int f2 = countBadO - countBadX;
-//        int evaluationScore = w1 * f1 + w2 * f2;
-//        return evaluationScore;
-//    };
+
 
     public int utility() {
         int countX = 0;
@@ -185,7 +139,7 @@ public class GameState {
         }
     }
 
-    public boolean isNodeValid(int i, int j) {
+    public static boolean isNodeValid(int i, int j) {
         return (i >= 0 && i < ROW && j >= 0 && j < ROW);
     }
     public void putX(int i, int j) {
